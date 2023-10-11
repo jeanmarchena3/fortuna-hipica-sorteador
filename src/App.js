bf553212eeaed0cfee45d80ae75b68f8e9e874c4
jeanmarchena3 <jeanmarchena31@gmail.com>
2023-10-11 15:17:38 -0400

    Animacion - funcionando en direccion vertical

--- src/App.js
+++ src/App.js
@@ -1,50 +1,61 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'antd';
 import './App.css';
 
-const App = () => (
-  <div className='App'>
-    <div className='main-container-info'>
-      <div className='container-info-reunion'>
-        <div className='container-data-reunion reunion'>
+const App = () => {
+  const [activateAnimation, setActivateAnimation] = useState(false);
+
+  const activarAnimacion = () =>{
+    console.log('activar animacion');
+    setActivateAnimation(true);
+  };
+
+  return (
+    <div className='App'>
+      <div className='main-container-info'>
+        <div className='container-info-reunion'>
+          <div className='container-data-reunion reunion'>
           SORTEO N 002
-        </div>
-        <div className='container-data-reunion m-l-10 dia-triple'>
+          </div>
+          <div className='container-data-reunion m-l-10 dia-triple'>
           MARTES
-        </div>
-        <div className='container-data-reunion m-l-10 fecha-triple'>
+          </div>
+          <div className='container-data-reunion m-l-10 fecha-triple'>
           06/10/2023
-        </div>
-        <div className='container-data-reunion m-l-10 hora-triple'>
+          </div>
+          <div className='container-data-reunion m-l-10 hora-triple'>
           1:00 PM
+          </div>
         </div>
       </div>
-    </div>
-    <div className='main-container-caballos'>
-      <div className='container-info-caballos'>
-        <div className='caballo1 m-b-20' id='output'>
-          <div className='m-l-10-percent text-caballos-numero'>0</div>
-          <div className='m-r-10-percent text-caballos-nombre'>Cañonero</div>
-        </div>
-        <div className='caballo2 m-b-20'>
-          <div className='m-l-10-percent text-caballos-numero'>0</div>
-          <div className='m-r-10-percent text-caballos-nombre'>Cañonero</div>
-        </div>
-        <div className='caballo3'>
-          <div className='m-l-10-percent text-caballos-numero'>0</div>
-          <div className='m-r-10-percent text-caballos-nombre'>Cañonero</div>
+      <div className='main-container-caballos'>
+        <div className='container-info-caballos'>
+          <div className='caballo1 m-b-20'>
+            <div className='test'>
+              <div className='m-l-10-percent text-caballos-numero'>0</div>
+              <div className='m-r-10-percent text-caballos-nombre'>Cañonero</div>
+            </div>
+          </div>
+          <div className='caballo2 m-b-20'>
+            <div className='test'>
+              <div className='m-l-10-percent text-caballos-numero'>0</div>
+              <div className='m-r-10-percent text-caballos-nombre'>Cañonero</div>
+            </div>
+          </div>
+          <div className='caballo3'>
+            <div className='test'>
+              <div className='m-l-10-percent text-caballos-numero'>0</div>
+              <div className='m-r-10-percent text-caballos-nombre'>Cañonero</div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div className='container'>
-      <div className='wrapper'>
-        <div className='sub-wrapper'>
-          <span className='word'>Huracan si</span>
-        </div>
+      <div>
+        <Button onClick={activarAnimacion}>Activar animacion</Button>
       </div>
     </div>
-    <div id='output'>--</div>
-  </div>
-);
+  );
+};
 
 export default App;
